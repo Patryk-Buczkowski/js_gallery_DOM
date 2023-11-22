9b3f6a1481b7fc60b9d2eedfa4ee31f6c4d1f0af
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-22 17:58:48 +0100

    make build

---
+++ index.32095c9b.js
@@ -0,0 +1,4 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").onclick=function(t){var r,a=t.target;"IMG"===a.tagName&&(t.preventDefault(),r=a.closest("a").getAttribute("href"),e&&(e.src=""),e.src=r);// return false;
+};//# sourceMappingURL=index.32095c9b.js.map
+
+//# sourceMappingURL=index.32095c9b.js.map
